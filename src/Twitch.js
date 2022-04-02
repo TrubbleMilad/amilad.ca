6be9ac046a442447ccd0a2c96949ea574fa78bcd
Anthony Milad <aoemilad@gmail.com>
2022-04-01 20:40:39 -0400

    New home page and twitch page

---
+++ src/Twitch.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function Twitch() {
+  return (
+    <div>
+      <header>
+        <h1><a href='/'>T</a>rubbleMilad</h1>
+      </header>
+      <body>
+        <p>Here is where I play twitch</p>
+      </body>
+    </div>
+  );
+}
+
+export default Twitch;
