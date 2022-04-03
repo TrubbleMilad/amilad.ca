72dcf13f6d37b3987f8e69126b1bf8b7a9ecaae2
Anthony Milad <aoemilad@gmail.com>
2022-04-03 19:39:42 -0400

    Nav bar and logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import ResponsiveAppBar from './NavMenu';
 
 function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar />
       <header className="App-header">
         <h1>Anthony Milad</h1>
       </header>
+      
     </div>
   );
 }
