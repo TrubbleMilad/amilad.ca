2695d13bc9891caeef4be31ac9ef205253b28f24
Anthony Milad <aoemilad@gmail.com>
2022-04-04 15:53:17 -0400

    Twitch Embed

--- src/Twitch.js
+++ src/Twitch.js
@@ -1,16 +1,26 @@
-import './App.css';
-
-function Twitch() {
+import './Twitch.css';
+import { TwitchEmbed, TwitchChat, TwitchClip, TwitchPlayer } from 'react-twitch-embed';
+ 
+const Stream = () => {
   return (
     <div>
       <header>
-        <h1>TrubbleMilad</h1>
+        <h1>TrubbleMilad's Twitch</h1>
       </header>
-      <body>
-        <p>Here is where I play twitch</p>
-      </body>
+      <TwitchEmbed
+        channel="trubblemilad"
+        id="trubblemilad"
+        theme="dark"
+        height="80vh"
+        width="100%"
+        muted
+        onVideoPause={() => console.log(':(')}
+      />
+      {/* <TwitchChat channel="trubblemilad" theme="dark" /> */}
+      {/* <TwitchClip clip="Highlight: Clutch Reconnection from Brand" parent={['amilad.ca']} /> */}
+      <TwitchPlayer video="1299628885" autoplay="false"/>
     </div>
   );
 }
 
-export default Twitch;
+export default Stream;
